51501916a5662e63aa86c05e2936ef576084a316
Popenkov Anton <popenkovaa@yandex.ru>
2022-10-08 14:48:06 +0300

    start

---
+++ app/js/utils.js
@@ -0,0 +1,14 @@
+function declOfNum(n, textForms) {
+  n = Math.abs(n) % 100;
+  var n1 = n % 10;
+  if (n > 10 && n < 20) {
+    return textForms[2];
+  }
+  if (n1 > 1 && n1 < 5) {
+    return textForms[1];
+  }
+  if (n1 === 1) {
+    return textForms[0];
+  }
+  return textForms[2];
+}
